36c7923c1cc78bfa2fced3d2e78e29cd4c739c9a
Woong <1406493495@qq.com>
2018-03-24 22:17:19 +0800

    [Bug XXXX] mainactivity can't scroll Cause: None Solution: None Fixed Version: None Branch: None

--- app/src/main/java/cn/com/woong/readhub/MainActivity.java
+++ app/src/main/java/cn/com/woong/readhub/MainActivity.java
@@ -62,7 +62,6 @@ public class MainActivity extends BaseActivity {
         mViewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(mViewPagerAdapter);
         viewPager.setCurrentItem(0);
-        viewPager.setCanScroll(true);
         rbTabTopic.setChecked(true);
         titleBar.setTitle(mTitleStrs.get(0));
 
