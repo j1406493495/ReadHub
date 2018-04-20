0a19bf283b0ca7995c918989a96035a4c1afc1ce
Woong <1406493495@qq.com>
2018-04-20 22:28:10 +0800

    [Bug XXXX] add agentweb Cause: None Solution: None Fixed Version: None Branch: None

---
+++ app/src/main/java/cn/com/woong/readhub/ui/news/NewsDetailActivity.java
@@ -0,0 +1,29 @@
+package cn.com.woong.readhub.ui.news;
+
+import com.just.agentweb.AgentWeb;
+
+import cn.com.woong.readhub.R;
+import cn.com.woong.readhub.base.BaseActivity;
+
+/**
+ * Created by wong on 2018/4/20.
+ */
+
+public class NewsDetailActivity extends BaseActivity {
+    AgentWeb mAgentWeb;
+
+    @Override
+    protected int getLayoutId() {
+        return R.layout.activity_news_detail;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
