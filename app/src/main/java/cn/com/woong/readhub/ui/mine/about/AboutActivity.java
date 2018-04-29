3afb5526d2b8a2ea87edaa3c798bcab9d5ef2bee
Woong <1406493495@qq.com>
2018-04-29 22:49:52 +0800

    [Bug XXXX] about activity Cause: None Solution: None Fixed Version: None Branch: None

---
+++ app/src/main/java/cn/com/woong/readhub/ui/mine/about/AboutActivity.java
@@ -0,0 +1,24 @@
+package cn.com.woong.readhub.ui.mine.about;
+
+import cn.com.woong.readhub.base.BaseActivity;
+
+/**
+ * Created by wong on 2018/4/29.
+ */
+
+public class AboutActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+}
