28557471e6f1701ab98f079f73597d98016228e2
Woong <1406493495@qq.com>
2018-04-30 22:54:57 +0800

    [Bug XXXX] xml activity about Cause: None Solution: None Fixed Version: None Branch: None

--- app/src/main/java/cn/com/woong/readhub/ui/mine/about/AboutActivity.java
+++ app/src/main/java/cn/com/woong/readhub/ui/mine/about/AboutActivity.java
@@ -1,15 +1,16 @@
 package cn.com.woong.readhub.ui.mine.about;
 
+import cn.com.woong.readhub.R;
 import cn.com.woong.readhub.base.BaseActivity;
 
 /**
+ * @author wong
  * Created by wong on 2018/4/29.
  */
-
 public class AboutActivity extends BaseActivity {
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.activity_about;
     }
 
     @Override
