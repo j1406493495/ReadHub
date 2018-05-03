2f98202c5c3473756d3770843d1e272e190de091
Woong <1406493495@qq.com>
2018-05-03 15:51:07 +0800

    [Bug XXXX] 友盟 Cause: None Solution: None Fixed Version: None Branch: None

--- app/src/main/java/cn/com/woong/readhub/App.java
+++ app/src/main/java/cn/com/woong/readhub/App.java
@@ -54,6 +54,9 @@ public class App extends Application implements HasSupportFragmentInjector, HasA
         DaggerAppComponent.create().inject(this);
 
         addApiService(GankioApiService.class);
+
+//        UMConfigure.init(this,"5a12384aa40fa3551f0001d1","umeng",UMConfigure.DEVICE_TYPE_PHONE,"");
+        //58edcfeb310c93091c000be2 5965ee00734be40b580001a0
     }
 
     public <T> void addApiService(Class<T> clz) {
---
+++ app/src/main/java/cn/com/woong/readhub/wxapi/WXEntryActivity.java
@@ -0,0 +1,10 @@
+package cn.com.woong.readhub.wxapi;
+
+import com.umeng.socialize.weixin.view.WXCallbackActivity;
+
+/**
+ * Created by wong on 2018/5/3.
+ */
+
+public class WXEntryActivity extends WXCallbackActivity {
+}
