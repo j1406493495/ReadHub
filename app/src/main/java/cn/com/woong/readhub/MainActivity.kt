10806168c638263fcfb08af59bbb3841632f4caf
Woong <1406493495@qq.com>
2019-02-01 22:27:20 +0800

    [Bug XXXX] main contentview Cause: None Solution: None Fixed Version: None Branch: None

--- app/src/main/java/cn/com/woong/readhub/MainActivity.kt
+++ app/src/main/java/cn/com/woong/readhub/MainActivity.kt
@@ -6,5 +6,6 @@ import android.support.v7.app.AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
     }
 }
